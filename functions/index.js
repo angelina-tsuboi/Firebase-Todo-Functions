d91ab101fbf3a3de3e01e85bb2c9e1ea3ca94cd1
angelina-tsuboi <angelina.t1832@gmail.com>
2020-07-03 06:42:37 -0700

    Todo API functionality complete

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,19 @@ const app = require('express')();
 
 const {
     getAllTodos,
-    postOneTodo
-} = require('./API/todos')
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./API/todos') //todo data stuff
+
+const {
+    loginUser
+} = require('./API/users') //user stuff
 
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
 
 exports.api = functions.https.onRequest(app);
 
